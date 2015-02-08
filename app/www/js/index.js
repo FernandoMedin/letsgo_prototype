a7a2a3f56cc53a4714afb1daab27742f6ee7160a
FernandoMedin <fernando-medin@hotmail.com>
2015-02-08 16:30:32 -0200

    Session with name

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,16 +1,12 @@
 var main = {
     get_name: function(){
-        var url = window.location.href;
-        var query_string = url.split("?");
-        var param_item = query_string[1].split("=");
-        var name = param_item[1];
-
-        return name;
+        return localStorage.name;
     },
 
     show_name: function(){
         var str = document.getElementById("name").innerHTML;
         var name = this.get_name();
+        console.log(name);
         var res = str.replace("nameID", name);
 
         document.getElementById("name").innerHTML = res;
@@ -31,6 +27,7 @@ var login = {
                     console.log(data);
                     alert('Now you are loged! Enjoy it!');
                     var name = data.replace(/\"/g, '');
+                    localStorage.name = name;
                     window.location.replace('main.html?name=' + name);
                 },
                 error: function(){
@@ -65,3 +62,27 @@ var login = {
     }
 };
 
+var events = {
+    create: function(){
+        $('form').submit(function(){
+            var landmarkID = $(this).parent().attr('data-landmark-id');
+            var postData = $(this).serialize();
+
+            $.ajax({
+                type: 'POST',
+                data: postData+'&amp;lid='+landmarkID,
+                url: 'http://localhost/lets_request/event.php',
+                success: function(data){
+                    console.log(data);
+                    alert('Your event was successfully created');
+                    window.location.replace('main.html');
+                },
+                error: function(){
+                    console.log(data);
+                    alert('There was an error creating your event');
+                }
+            });
+        return false;
+        });
+    }
+};
