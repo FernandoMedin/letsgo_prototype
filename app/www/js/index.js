016a7d4cbba96cf358c9a43e603c20766512e697
FernandoMedin <fernando-medin@hotmail.com>
2015-02-21 01:11:38 -0200

    AngularJS

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,24 +1,3 @@
-var main = {
-    get_name: function(){
-        return localStorage.name;
-    },
-
-    show_name: function(){
-        var str = document.getElementById("name").innerHTML;
-        var name = this.get_name();
-        console.log(name);
-        var res = str.replace("nameID", name);
-
-        document.getElementById("name").innerHTML = res;
-    },
-
-    show_user: function(){
-        document.getElementById("name").innerHTML = localStorage.name;
-        document.getElementById("last_name").innerHTML = localStorage.last_name;
-        document.getElementById("email").innerHTML = localStorage.email;
-    },
-};
-
 var login = {
     auth: function(){
         $('form').submit(function(){
@@ -107,30 +86,5 @@ var events = {
             });
         return false;
         });
-    }
-};
-
-var data = {
-    get_user: function(){
-        $.ajax({
-            type: 'POST',
-            data: 'id_user=' + localStorage.id_user + '&passwd=' + localStorage.passwd,
-            url: 'http://localhost:8000/get_user_data/',
-            success: function(data){
-                console.log(data);
-                if(data == "Error"){
-                    alert('Error');
-                    localStorage.clear();
-                    window.location.replace('index.html');
-                }else{
-                    alert("We found your data!");
-                }
-            },
-            error: function(){
-                console.log(data);
-                alert('Database Error');
-            }
-        });
-    return false;
     }
 };
